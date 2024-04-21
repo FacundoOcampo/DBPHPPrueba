218c90339f79fda0f6cb847e8856a8018aafc7a2
FacundoOcampo <103945027+FacundoOcampo@users.noreply.github.com>
2024-04-21 17:26:07 -0300

    Delete tbl_colores.sql

--- tbl_colores.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO `tbl_colores` (`id`, `nombre`, `descripcion`) VALUES (1, 'danger', 'este color es rojo');
-INSERT INTO `tbl_colores` (`id`, `nombre`, `descripcion`) VALUES (2, 'warning', 'este color es amarillo');
-INSERT INTO `tbl_colores` (`id`, `nombre`, `descripcion`) VALUES (3, 'success', 'este color es verde');
-INSERT INTO `tbl_colores` (`id`, `nombre`, `descripcion`) VALUES (4, 'info', 'este color es celeste');
-INSERT INTO `tbl_colores` (`id`, `nombre`, `descripcion`) VALUES (5, 'dark', 'este color es negro');
-INSERT INTO `tbl_colores` (`id`, `nombre`, `descripcion`) VALUES (6, 'dark', 'este color es negro');
